cf835df6a25effd9c4bda003c6404cc1a35978cd
Richard McGain <mcgain@gmail.com>
2012-07-23 15:46:46 -0400

    inject static_handlers and list_handlers into router

--- node/index.js
+++ node/index.js
@@ -1,4 +1,5 @@
 var server = require("./server");
 var router = require("./router");
-
-server.start(router.route);
+var static_handler = require("./static_handler");
+var list_handler = require("./list_handler");
+server.start(router.route, static_handler, list_handler);
--- node/server.js
+++ node/server.js
@@ -1,15 +1,11 @@
 var http = require("http");
 var url = require("url");
 
-function start(route) {
+function start(route, static_handler, list_handler) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + "received.");
-
-    route(pathname);
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
+    console.log("Request for " + pathname + " received.");
+    route(pathname static_handler, list_handler);
     response.end();
   }
 
