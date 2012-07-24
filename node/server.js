12beccd764b41d94034825600f405857b8a7ec13
Richard McGain <mcgain@gmail.com>
2012-07-24 12:54:10 -0400

    initial handler implementation

--- node/server.js
+++ node/server.js
@@ -5,7 +5,9 @@ function start(route, static_handler, list_handler) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
+    
     route(pathname static_handler, list_handler);
+
     response.end();
   }
 
