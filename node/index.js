1daf2de5f9c67d7e19cc74eb1f34bf00f6617ecb
Richard McGain <mcgain@gmail.com>
2012-07-30 11:13:47 -0400

    removed node in preperation for meteor

--- node/index.js
+++
@@ -1,5 +0,0 @@
-var server = require("./server");
-var router = require("./router");
-var static_handler = require("./static_handler");
-var list_handler = require("./list_handler");
-server.start(router.route, static_handler, list_handler);
