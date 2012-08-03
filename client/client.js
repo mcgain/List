d83775de3346cf43e917d19c390476ccfc8f2d35
Richard McGain <mcgain@gmail.com>
2012-08-03 14:27:48 -0400

    persist changes to the table

--- client/client.js
+++ client/client.js
@@ -1,6 +1,28 @@
-var data = Meteor.subscribe("datatable", "something");
-var Table = new Meteor.Collection("datatable");
-var entries = Table.find("something");
-$("#main-table").handsontable({rows: 2, cols: 3});
-$("#main-table").handsontable("loadData", entries);
+var Table = null;
+var data = Meteor.subscribe("datatable", "something", function(){
+  Table = new Meteor.Collection("datatable");
+  var entries = Table.findOne().table;
+  $("#main-table").handsontable({
+    rows: 2, 
+    cols: 3,
+    onChange: updateDatasource
+  });
+  $("#main-table").handsontable("loadData", entries);
+});
+
+var updateDatasource = function(data, source) {
+  if (source === "loadData") {
+    return;
+  }
+  var row = data[0][0];
+  var col = data[0][1];
+  var change = data[0][3];
+  var entries = Table.findOne().table;
+  console.log("Entries: " + entries + " Change: " + change + " col:" + col + " row: " + row);
+  entries[row][col] = change;
+  console.log("XXXX Entries: " + entries + " Change: " + change);
+  Table.update({table_id: "something"}, {$set: {table: entries}});
+  $("#main-table").handsontable("loadData", entries);
+  console.log("inserting " + change);
+};
 
