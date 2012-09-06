6bff70d72cb90d23c5860e390e0a4c4920f7d67b
Richard McGain <mcgain@gmail.com>
2012-09-06 14:19:44 -0400

    added grunt

---
+++ .grunt/grunt.js
@@ -0,0 +1,52 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    lint: {
+      files: ['grunt.js', '../server/*.js', '../client/*.js', '../tests/**/*.js']
+    },
+    qunit: {
+      files: ['tests/**/*.html']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit'
+    },
+    jshint: {
+      options: {
+        bitwise: true,
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        forin: true,
+        indent: 2,
+        nonew: true,
+        regexp: true,
+        unused: true,
+        maxdepth: 3,
+        maxstatements: 7,
+        jquery: true,
+        node: true
+      },
+      globals: {
+        Meteor: true,
+        Template: true, 
+        Backbone: true,
+        Session: true
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint qunit');
+
+};
