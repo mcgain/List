526fb0f9f95b43b5c5a8f00962e8c0af5b7bf70b
Richard McGain <mcgain@gmail.com>
2012-09-09 16:59:53 -0400

    router must load first'

--- client/client.js
+++ client/client.js
@@ -1,11 +1,29 @@
 (function() {
+  var ListsRouter = Backbone.Router.extend({
+    routes: {
+      ":list_name": "main"
+    },
+    main: function (list_id) {
+      Session.set("list_id", list_id);
+    },
+    setList: function (list_id) {
+      this.navigate(list_id, true);
+    }
+  });
+
+  var Router = new ListsRouter();
+
+  Backbone.history.start({pushState: true});
+
   var Table = null;
-  var data = Meteor.subscribe("datatable", "something", function(){
+
+  var datatable_id = Session.get("list_id");
+  var data = Meteor.subscribe("datatable", datatable_id, function() {
     Table = new Meteor.Collection("datatable");
     var entries = Table.findOne().table;
     $("#main-table").handsontable({
-      rows: 2, 
-      cols: 3,
+      rows: 3, 
+      cols: 1,
       minSpareCols: 1,
       minSpareRows: 1,
       onChange: updateDatasource
@@ -31,6 +49,7 @@
   };
 
   var insertData = function(row, col, value, entries) {
+    console.log("insert data called");
     if(entries[row] === undefined) {
       var newRow = []; 
       newRow[col] = value; 
@@ -41,20 +60,4 @@
     }
   };
 
-  var ListsRouter = Backbone.Router.extend({
-    routes: {
-      ":list_name": "main"
-    },
-      main: function (list_id) {
-        Session.set("list_id", list_id);
-      },
-      setList: function (list_id) {
-        console.log("setList called");
-        this.navigate(list_id, true);
-      }
-  });
-
-  var Router = new ListsRouter();
-
-  Backbone.history.start({pushState: true});
 }());
